9c1fe96da3677c31fa8b8e706b3d179e8b6b4346
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-04-27 18:11:28 +0500

    feat: add Keyboard class
    added a keyboard class in which there are methods for creating
    a keyboard and handling keystrokes

---
+++ src/keyboard/modules/Keyboard.js
@@ -0,0 +1,129 @@
+import { keys, keysMap } from '../../data/keys-data';
+import Key from './Key';
+
+/* eslint-disable no-param-reassign */
+class Keyboard {
+  constructor() {
+    this.keys = [];
+    this.classesCSS = {
+      KEYS_KEYBOARD: 'keyboard',
+      KEYBOARD_ROW: 'keyboard__row',
+    };
+    this.pressedKey = null;
+    this.capsPressed = false;
+    this.altPressed = false;
+    this.shiftPressed = false;
+    this.currentLanguage = localStorage.getItem('language');
+  }
+
+  createKeyboard() {
+    const currentLanguage = localStorage.getItem('language');
+    const keyboard = document.createElement('div');
+    keyboard.classList.add(this.classesCSS.KEYS_KEYBOARD);
+
+    let newRow = null;
+    let newKey = null;
+
+    for (let row = 0; row < keys.length; row += 1) {
+      newRow = document.createElement('div');
+      newRow.classList.add(this.classesCSS.KEYBOARD_ROW);
+      for (let key = 0; key < keys[row].length; key += 1) {
+        newKey = new Key(keys[row][key]).createKey(currentLanguage);
+        this.keys.push(newKey);
+        newRow.append(newKey);
+      }
+      keyboard.append(newRow);
+    }
+    return keyboard;
+  }
+
+  langChangeHandlerPressDown(event) {
+    if (event.code === 'AltLeft') {
+      this.altPressed = true;
+    }
+    if (event.code === 'ShiftLeft') {
+      this.shiftPressed = true;
+    }
+    if (this.shiftPressed && this.altPressed) {
+      const newLanguage = this.currentLanguage === 'en' ? 'ru' : 'en';
+      this.currentLanguage = newLanguage;
+      localStorage.setItem('language', newLanguage);
+      this.keys.forEach((key) => {
+        const { keyCode } = key.dataset;
+        key.firstChild.textContent = keysMap.get(keyCode)[newLanguage].text;
+      });
+    }
+  }
+
+  langChangeHandlerPressUp(event) {
+    if (event.code === 'AltLeft') {
+      this.altPressed = false;
+    }
+    if (event.code === 'ShiftLeft') {
+      this.shiftPressed = false;
+      this.handleShiftState();
+    }
+  }
+
+  turnUpSymbolsPressDown(event) {
+    if (event.code === 'ShiftLeft') {
+      this.shiftPressed = true;
+      this.handleShiftState();
+    }
+    if (event.code === 'CapsLock') {
+      this.capsPressed = !this.capsPressed;
+      this.handleCapsState();
+    }
+  }
+
+  turnUpSymbolsPressUp(event) {
+    if (event.code === 'ShiftLeft') {
+      this.shiftPressed = false;
+      this.handleShiftState();
+    }
+  }
+
+  handleShiftState() {
+    this.keys.forEach((key) => {
+      const { keyCode } = key.dataset;
+      if (this.shiftPressed && !this.capsPressed) {
+        key.firstChild.textContent = keysMap.get(keyCode)[this.currentLanguage].textShift;
+      } else {
+        key.firstChild.textContent = keysMap.get(keyCode)[this.currentLanguage].text;
+      }
+    });
+  }
+
+  handleCapsState() {
+    this.keys.forEach((key) => {
+      const { keyCode } = key.dataset;
+      if (this.capsPressed && !this.shiftPressed) {
+        key.firstChild.textContent = keysMap.get(keyCode)[this.currentLanguage].textCaps;
+      } else if (this.shiftPressed && this.capsPressed) {
+        key.firstChild.textContent = keysMap.get(keyCode)[this.currentLanguage].textShift;
+      } else {
+        key.firstChild.textContent = keysMap.get(keyCode)[this.currentLanguage].text;
+      }
+    });
+  }
+
+  pressDownHandler(event) {
+    const preventDefaultKeys = ['Tab', 'AltLeft', 'AltRight', 'ShiftLeft', 'ShiftRight'];
+    if (preventDefaultKeys.includes(event.code)) {
+      event.preventDefault();
+    }
+    this.pressedKey = this.keys.find((key) => key.dataset.keyCode === event.code);
+    this.pressedKey.classList.add('active');
+    this.langChangeHandlerPressDown(event);
+    this.turnUpSymbolsPressDown(event);
+  }
+
+  pressUpHandler(event) {
+    this.pressedKey = this.keys.find((key) => key.dataset.keyCode === event.code);
+    this.pressedKey.classList.remove('active');
+    this.langChangeHandlerPressUp(event);
+    this.turnUpSymbolsPressUp(event);
+  }
+}
+
+export default Keyboard;
